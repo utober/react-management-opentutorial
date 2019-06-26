ec854403de128f4ba0446aec993cdb51cef1bafd
utober <utober77@gmail.com>
2019-06-26 11:40:36 +0900

    Implement Customer Add & Customer List Refresh Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './App.css';
 import Customer from './components/Customer';
+import './App.css';
+import CustomerAdd from './components/CustomerAdd';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
@@ -25,17 +26,38 @@ const styles = theme => ({
 })
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0
+    };
+  }
 
-  state = {
-    customers: "",
-    completed: 0
+  stateRefresh = () => {
+    this.setState({
+      customers:'',
+      completed: 0
+    });
+    this.callApi()
+    .then(res => {
+        this.setState({customers: res})
+      })
+    .catch(err => console.log(err));
   }
 
   componentDidMount() {
     this.timer = setInterval(this.progress, 20);
     this.callApi()
-      .then(res => this.setState({customers: res}))
+      .then(res => {
+          this.setState({customers: res})
+          clearInterval(this.timer);
+        })
       .catch(err => console.log(err));
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
   }
 
   callApi = async () => {
@@ -52,41 +74,45 @@ class App extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.customers ? this.state.customers.map(c => {
-              return (
-                <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />
-              )
-            }) : 
-            <TableRow>
-              <TableCell colSpan="6" align="center">
-                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
-              </TableCell>
-            </TableRow>
-            }
-          </TableBody>
-        </Table>
-      </Paper>
+      <div>
+        <CustomerAdd stateRefresh={this.stateRefresh} />
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? 
+              this.state.customers.map(c => {
+                return (
+                  <Customer
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                )
+              }) : 
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                </TableCell>
+              </TableRow>
+              }
+            </TableBody>
+          </Table>
+        </Paper>
+      </div>
     );
   }
 }
--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -5,16 +5,14 @@ import TableCell from '@material-ui/core/TableCell';
 class Customer extends React.Component {
   render() {
     return (
-      <>
-        <TableRow>
-          <TableCell>{this.props.id}</TableCell>
-          <TableCell><img src={this.props.image} alt="profile" /></TableCell>
-          <TableCell>{this.props.name}</TableCell>
-          <TableCell>{this.props.birthday}</TableCell>
-          <TableCell>{this.props.gender}</TableCell>
-          <TableCell>{this.props.job}</TableCell>
-        </TableRow>
-      </>
+      <TableRow>
+        <TableCell>{this.props.id}</TableCell>
+        <TableCell><img src={this.props.image} alt="profile" /></TableCell>
+        <TableCell>{this.props.name}</TableCell>
+        <TableCell>{this.props.birthday}</TableCell>
+        <TableCell>{this.props.gender}</TableCell>
+        <TableCell>{this.props.job}</TableCell>
+      </TableRow>
     );
   };
 }
